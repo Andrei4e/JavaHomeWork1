579ee53adc0c08e17bca48ebbbba7d04ff4d43bc
Andrei <Andrei@mail.ru>
2023-02-19 22:56:06 +0300

    Готовое домашнее задание

--- javahomework1.java
+++ javahomework1.java
@@ -1,9 +1,24 @@
 //Написать программу вычисления n-ого треугольного числа
+import java.util.Scanner;
 
 public class javahomework1 {
 
     public static void main(String[] args) 
     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Введите целое число n: ");
+        int n = scanner.nextInt();
         
+        int result = triangularNumber(n);
+
+        System.out.print(result);
+        scanner.close(); 
+    }
+
+    //метод вычисления n-го члена треугольного числа
+    static int triangularNumber(int n)
+    {
+        int result = n * (n + 1) / 2;
+        return result;
     }
 }
